ce239563eb761f26622d01a7fa8e3bd249efe21a
mah454 <mah454@gmail.com>
2023-11-25 20:57:53 +0330

    Fix upgrade version

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(useMaterial3: false),
       debugShowCheckedModeBanner: false,
       title: 'JOS',
       initialRoute: '/',
