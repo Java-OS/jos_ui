66dd6a380c18fb75e6258d52f70626d6bb04f3be
Mahdi <mah454@mail.moke.ir>
2024-05-21 02:30:28 +0330

    Fix make-web.sh

--- make-web.sh
+++ make-web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 rm -rf build/ 
-flutter build web --web-renderer=canvaskit --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/
+flutter clean ; flutter build web --web-renderer=canvaskit --dart-define=FLUTTER_WEB_CANVASKIT_URL=/canvaskit/
