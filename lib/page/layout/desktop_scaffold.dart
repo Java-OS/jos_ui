ac7da55fc1050cdd5d1c9eab673548111de3f99c
Mahdi <mah454@mail.moke.ir>
2025-04-02 15:34:18 +0330

    fix logout button

--- lib/page/layout/desktop_scaffold.dart
+++ lib/page/layout/desktop_scaffold.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:jos_ui/component/drawer.dart';
 import 'package:jos_ui/controller/authentication_controller.dart';
 import 'package:jos_ui/dialog/power_dialog.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class DesktopScaffold extends StatefulWidget {
   final Widget content;
@@ -37,8 +36,8 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
                     actions: [
                       MouseRegion(
                         cursor: SystemMouseCursors.click,
-                        onHover:(e) => setState(() => _hoverOnPower = true),
-                        onExit:(e) => setState(() => _hoverOnPower = false),
+                        onHover: (e) => setState(() => _hoverOnPower = true),
+                        onExit: (e) => setState(() => _hoverOnPower = false),
                         child: GestureDetector(
                           onTap: () => displayPowerModal(),
                           child: AnimatedContainer(
@@ -46,20 +45,23 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
                             color: _hoverOnPower ? Colors.blue : Colors.transparent,
                             width: 60,
                             duration: Duration(milliseconds: 600),
-                            child: Icon(Icons.power_settings_new_outlined,color: _hoverOnPower ? Colors.white : Colors.black),
+                            child: Icon(Icons.power_settings_new_outlined, color: _hoverOnPower ? Colors.white : Colors.black),
                           ),
                         ),
                       ),
                       MouseRegion(
                         cursor: SystemMouseCursors.click,
-                        onHover:(e) => setState(() => _hoverOnLogout = true),
-                        onExit:(e) => setState(() => _hoverOnLogout = false),
-                        child: AnimatedContainer(
-                          curve: Curves.easeInOut,
-                          color: _hoverOnLogout ? Colors.blue : Colors.transparent,
-                          width: 60,
-                          duration: Duration(milliseconds: 600),
-                          child: Icon(Icons.logout,color: _hoverOnLogout ? Colors.white : Colors.black),
+                        onHover: (e) => setState(() => _hoverOnLogout = true),
+                        onExit: (e) => setState(() => _hoverOnLogout = false),
+                        child: GestureDetector(
+                          onTap: () => _authController.logout(),
+                          child: AnimatedContainer(
+                            curve: Curves.easeInOut,
+                            color: _hoverOnLogout ? Colors.blue : Colors.transparent,
+                            width: 60,
+                            duration: Duration(milliseconds: 600),
+                            child: Icon(Icons.logout, color: _hoverOnLogout ? Colors.white : Colors.black),
+                          ),
                         ),
                       ),
                     ],
