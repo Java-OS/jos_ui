f8b7bcc0e8ae6052918e3de742e2e1a0431473a8
Mahdi <mah454@mail.moke.ir>
2025-04-25 18:47:16 +0330

    fix null

--- lib/model/module.dart
+++ lib/model/module.dart
@@ -4,9 +4,12 @@ class Module {
   final String name;
   final bool started;
   final String path;
+  final String? description;
+  final String? maintainer;
+  final String? url;
   final List<ModuleDependency> dependencies;
 
-  Module({required this.name, required this.started, required this.path, required this.dependencies});
+  Module({required this.name, required this.started, required this.path, required this.description, required this.maintainer, required this.url, required this.dependencies});
 
   factory Module.fromMap(Map<String, dynamic> json) {
     var deps = (json['dependencies'] as List).map((e) => ModuleDependency.fromMap(e)).toList();
@@ -14,6 +17,9 @@ class Module {
       name: json['name'],
       started: json['started'],
       path: json['path'],
+      description: json['description'] ?? '',
+      maintainer: json['maintainer'] ?? '',
+      url: json['url'] ?? '',
       dependencies: deps,
     );
   }
